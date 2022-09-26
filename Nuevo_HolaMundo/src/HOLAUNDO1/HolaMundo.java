51ed456b650214e2895180e9b0b2951b5e4c3030
6002297 <6002297@PC29ATP8.insags.com>
2022-09-26 09:01:19 +0200

    Nuevo Hola Mundo

---
+++ Nuevo_HolaMundo/src/HOLAUNDO1/HolaMundo.java
@@ -0,0 +1,20 @@
+package HOLAUNDO1;
+
+class HolaMundo {
+	/**
+	 * @author 6002297 Haciendo uso de la constante
+	 */
+
+	// se añadio la constante
+	private static final String HolaMundo = "hola mundo";
+
+	public static void main(String[] args) {
+	/**
+	* Probando ejercicio basico
+	 */
+	// comentario hola mundo
+	System.out.println(HolaMundo);
+
+	}
+
+}
---
+++ PROYECTO_Holamundo/src/PROYECTO_Holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+package PROYECTO_Holamundo;
+
+public class HolaMundo {
+	/**
+	 * @author 6002297 Haciendo uso de la constante
+	 */
+
+	// se añadio la constante
+	private static final String HolaMundo = "hola mundo";
+
+	public static void main(String[] args) {
+		/**
+		 * Probando ejercicio basico
+		 */
+		// comentario hola mundo
+		System.out.println(HolaMundo);
+	}
+
+}
